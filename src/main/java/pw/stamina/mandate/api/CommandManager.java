214f10e7dc818dd00d9b62d56792a5ccb3b072c0
foundry27 <xfoundry27@yahoo.ca>
2016-07-11 23:05:46 -0400

    Added JavaDoc comments to the CommandManager class

--- src/main/java/pw/stamina/mandate/api/CommandManager.java
+++ src/main/java/pw/stamina/mandate/api/CommandManager.java
@@ -18,6 +18,7 @@
 
 package pw.stamina.mandate.api;
 
+import pw.stamina.mandate.api.annotations.Syntax;
 import pw.stamina.mandate.api.component.SyntaxComponent;
 import pw.stamina.mandate.api.execution.argument.ArgumentHandler;
 import pw.stamina.mandate.api.execution.result.Execution;
@@ -27,16 +28,61 @@ import java.util.Collection;
 import java.util.Optional;
 
 /**
+ * A central manager and executor for a collection of registered commands
+ * A CommandManager is used to coordinate much of the process of using Mandate, including providing a
+ * central hub for registering commands, handling the parsing and eventual execution of user input, and
+ * allowing external components to interact with the command ecosystem
+ *
  * @author Foundry
  */
 public interface CommandManager {
+
+    /**
+     * Registers all correctly defined commands in the specified container object
+     * A command method is considered to be correctly defined if either of these requirements are met:
+     *  - It is flagged as an {@link pw.stamina.mandate.api.annotations.Executes executor} and has its own specific {@link Syntax Syntax} annotation
+     *  - It is flagged as an {@link pw.stamina.mandate.api.annotations.Executes executor} and it's containing class has a global {@link Syntax Syntax} annotation
+     *  If duplicate commands exist in a registered container, a CommandManager implementation should take steps to prevent those commands from being re-added to the
+     *  command registry
+     *
+     * @param container the object containing the commands to be registered
+     * @return {@code true} if any valid commands were registered, else {@code false}
+     */
     boolean register(Object container);
 
+    /**
+     * Tries to execute a command based on the provided input
+     * If the execution fails as a result of invalid or malformed user input, the result of this
+     * will immediately have a present exit code of {@link pw.stamina.mandate.api.execution.result.ExitCode#INVALID INVALID}. Otherwise,
+     * this will return an {@link Execution execution} corresponding to the evaluated command running possibly asynchronously
+     *
+     * @param input the input to be tokenized and parsed as a command and it's arguments
+     * @return a running execution if the invocation completed without errors, else a completed execution with an exit code of INVALID
+     */
     Execution execute(String input);
 
+    /**
+     * Tries to execute a command based on the provided input, using the provided IODescriptor for the execution
+     * If the execution fails as a result of invalid or malformed user input, the result of this
+     * will immediately have a present exit code of {@link pw.stamina.mandate.api.execution.result.ExitCode#INVALID INVALID}. Otherwise,
+     * this will return an {@link Execution execution} corresponding to the evaluated command running possibly asynchronously
+     *
+     * @param input the input to be tokenized and parsed as a command and it's arguments
+     * @return a running execution if the invocation completed without errors, else a completed execution with an exit code of INVALID
+     */
     Execution execute(String input, IODescriptor descriptor);
 
+    /**
+     * Attempts to find an {@link ArgumentHandler ArgumentHandler} capable of handling the provided type
+     *
+     * @param type the type for which an argument handler should be looked up
+     * @param <T> the type of value returned by the searched argument handler
+     * @return an ArgumentHandler capable of handling the provided type if one is present, else {@link Optional#empty() an empty Optional}
+     */
     <T> Optional<ArgumentHandler<T>> findArgumentHandler(Class<T> type);
 
+    /**
+     * @return a collection of all the base syntax components from which valid commands exist
+     */
     Collection<SyntaxComponent> getCommands();
 }
